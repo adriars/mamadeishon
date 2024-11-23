419ab3c745cb5d0ca8c9766f5e46e3ef05f2ca84
Nicolas Fadul <nicolas.fadul@gmail.com>
2024-11-23 17:24:23 +0100

    Training done bitch

--- scripts/body_part.gd
+++ scripts/body_part.gd
@@ -5,9 +5,9 @@ var condition : int = 0;
 var soreness : int = 0;
 var fat : int = 0;
 var rested : bool = false;
-
-
-
+var soft_training : Training = Training.new(4,20,0.5,15);
+var mid_training : Training = Training.new(4,20,0.5,35);
+var hard_training : Training = Training.new(4,20,0.5,70);
 
 func _init(name: String) -> void:
 	name = name
@@ -16,8 +16,14 @@ func _init(name: String) -> void:
 	fat = 0
 	rested = 0
 
-func train(training : int) -> void:
-		if
+func traing(training : int) -> void:
+	if training = 0:
+		soft_training.training(self)
+	if training = 1:
+		mid_training.training(self)
+	if training = 2:
+		hard_training.training(self)		
+
 	
 
 func add_soreness(soreness : int) -> void:
@@ -34,28 +40,30 @@ func add_fat(ammount : int) -> void:
 func rest() -> void:
 	self.soreness = 0;
 
-
-class training:
+class Training:
 	var condition:int = 0;
 	var soreness:int = 0;
 	var fat:int = 0;
+	var energy:int = 0;
 	
-	func _init(condition:int, soreness:int, fat:int):
+	func _init(condition:int, soreness:int, fat:int, energy:int):
 		self.condition = condition
 		self.soreness = soreness
 		self.fat = fat
 	
-	func improve_training(money):
+	func improve_training():
 		if GymBro.money > self.condition*10:
 			GymBro.money -= money
 			self.condition += 10
-			self.fat + =5
+			self.fat += 5
+			self.energy -= 3
 		else:
 			print("no money bicth")
 	
-	func easy_training():
-		
-		
-	func mid_training():
-		
-	func hard_push():
+	func training(bodypart : BodyPart):
+		if(Gymbro.energy > self.energy):
+			GymBro.energy -= self.energy * energy
+			bodypart.add_codition(self.condition)
+			bodypart.add_soreness(self.soreness)
+			bodypart.add_fat(-self.fat)
+			
